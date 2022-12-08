a9a7ac1509a419692dcd6ce8b85721ffbf63e993
Yousef <mr.ysaj@gmail.com>
2022-12-08 11:58:27 +0330

    [UPDATE] part-A lesson was finished

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 #Data Source
 import yfinance as yf
+from dash import Dash, html,dcc
+import pandas as pd
+import plotly.express as px
 
-from dash import Dash, html
+happiness = pd.read_csv("13 - worldhappiness.csv")
+region = happiness["region"].unique()
+country = happiness["country"].unique()
 
 app = Dash()
 
-app.layout = html.Div("My Dashboard")
+app.layout = html.Div([
+    html.H1("World Happiness Dashboard"),
+    html.P(["This dashboard shows the happiness score.",
+           html.Br(),
+           html.A("World Happiness Report Data Source",
+                  href="https://worldhappiness.report",
+                  target="_blank")]),
+    dcc.RadioItems(options=region, value="North America"),
+    dcc.Checklist(options=region, value=["North America"]),
+    dcc.Dropdown(options=country, value="United State"),
+    dcc.Graph(figure=px.line(happiness[happiness["country"] == "United States"],
+                             x="year", y="happiness_score",
+                             title="Happiness Score in the USA"))
+])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
