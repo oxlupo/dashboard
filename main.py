363909c0dcd383940bd959ee421e4297e2e6725b
Yousef <mr.ysaj@gmail.com>
2022-12-08 12:18:28 +0330

    [UPDATE] Add callback func

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Data Source
 import yfinance as yf
-from dash import Dash, html,dcc
+from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
 
@@ -11,19 +11,17 @@ country = happiness["country"].unique()
 app = Dash()
 
 app.layout = html.Div([
-    html.H1("World Happiness Dashboard"),
-    html.P(["This dashboard shows the happiness score.",
-           html.Br(),
-           html.A("World Happiness Report Data Source",
-                  href="https://worldhappiness.report",
-                  target="_blank")]),
-    dcc.RadioItems(options=region, value="North America"),
-    dcc.Checklist(options=region, value=["North America"]),
-    dcc.Dropdown(options=country, value="United State"),
-    dcc.Graph(figure=px.line(happiness[happiness["country"] == "United States"],
-                             x="year", y="happiness_score",
-                             title="Happiness Score in the USA"))
+    dcc.Input(id="input_text", value="Change this text",
+              type="text"),
+    html.Div(id="output_text")
 ])
 
+@app.callback(
+    Output(component_id="output_text", component_property="children"),
+    Input(component_id="input_text", component_property="value")
+)
+def update_output_div(input_text):
+    """update text"""
+    return f"text:{input_text}"
 if __name__ == "__main__":
     app.run_server(debug=True)
