79f841b454a8499b4bfeadc0ad1d2e44aa8c8f1d
Yousef <mr.ysaj@gmail.com>
2022-12-15 13:14:04 +0330

    [UPDATE] average func and more callback wass added

--- main.py
+++ main.py
@@ -18,27 +18,50 @@ app.layout = html.Div([
             html.A("World happiness report Data source",
                    href="https://worldhappiness.report",
                    target="_blank")]),
+    dcc.RadioItems(id="region-radio",
+                   options=happiness["region"].unique(),
+                   value="North America"),
 
     dcc.Dropdown(id="country-dropdown",
         options=happiness["country"].unique(),
         value="United States"),
-    dcc.Graph(id="happiness-graph",)
+    dcc.RadioItems(id="data-radio",
+                   options={
+                      "Happiness_score:": "Happiness_score",
+                      "Happiness_rank:": "Happiness_rank"
+                   },
+                   value="happiness_score"),
+    dcc.Graph(id="happiness-graph",),
+    html.Div(id="average-div")
 ])
 
 
 @app.callback(
-    Output(component_id="happiness-graph", component_property="figure"),
-    Input(component_id="country-dropdown", component_property="value")
+    Output("country-dropdown", "option"),
+    Output("country-dropdown", "value"),
+    Input("region-radio", "value")
 )
+def update_dropdown(selected_region):
+    filtered_happiness = happiness[happiness["country"] == selected_region]
+    country_option=filtered_happiness["country"].unique()
+    return country_option, country_option[0]
 
-def update_graph(selected_country):
+
+@app.callback(
+    Output(component_id="happiness-graph", component_property="figure"),
+    Output(component_id="average-div", component_property='children'),
+    Input(component_id="country-dropdown", component_property="value"),
+    Input(component_id="data-radio", component_property="value"))
+def update_graph(selected_country, selected_data):
     """update the country that choose"""
     filtered_happiness = happiness[happiness["country"] == selected_country]
     line_fig = px.line(filtered_happiness,
                      x="year",
-                     y="happiness_score",
-                     title=f'Happiness Score in {selected_country}')
-    return line_fig
+                     y=selected_data,
+                     title=f'{selected_data} in {selected_country}')
+    selected_avg = filtered_happiness[selected_data].mean()
+    return line_fig, f"The average {selected_data} for {selected_country}" \
+                     f"is{selected_avg}"
 
 
 def update_output_div(input_text):
