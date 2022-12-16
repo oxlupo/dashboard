e43dcf97f1b71b08953391fe7b4956422907acd2
Yousef <mr.ysaj@gmail.com>
2022-12-16 21:56:26 +0330

    [Update] add def of dataframe_coin

--- live_price.py
+++ live_price.py
@@ -1,15 +1,21 @@
 from pycoingecko import CoinGeckoAPI
+import pandas as pd
+
 cg = CoinGeckoAPI()
-
-# btc = cg.get_price(ids='bitcoin', vs_currencies='usd')
-# print(btc)
-
-gh = cg.get_price(ids='bitcoin,fantom,ethereum', vs_currencies='usd,eur')
-print(gh)
 
 
 def get_price_token(token_name, pair):
     """get price of a token or Coin at the moment"""
-    return cg.get_price(ids=token_name, vs_currencies=pair)
+    if not isinstance(token_name, dict):
+
+        return cg.get_price(ids=token_name, vs_currencies=pair)
+    else:
+        raise Exception("An Error was occur don't use Dict for token_list")
 
 
+def dataframe_coin():
+    """all tokens and coin in table:return"""
+
+    coin_list = cg.get_coins_list()
+    coin_df = pd.DataFrame.from_dict(coin_list).sort_values("id").reset_index(drop=True)
+    return coin_df
