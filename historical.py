ca6c87a693bd25113eac5fbaae3ec72ec6218e94
yousef <mr.ysaj@gmail.com>
2023-03-27 10:44:33 +0330

    [UPDATE] add get historical_data function

--- historical.py
+++ historical.py
@@ -20,3 +20,31 @@ def dataframe_coin():
     coin_df = pd.DataFrame.from_dict(coin_list).sort_values("id").reset_index(drop=True)
     return coin_df
 
+
+def get_historical_data(id: str, days: str, interval: str, vs_currency: str):
+    """
+    id='bitcoin'
+    days= '150'
+    interval= 'daily'
+    vs_currency= 'usd'
+    """
+
+    try:
+        token_hist = cg.get_coin_market_chart_by_id(id=id, vs_currency=vs_currency, days=days, interval=interval)["prices"]
+        historical_list = list(map(lambda index: index[1], token_hist))
+        return historical_list
+    except Exception as e:
+        print(e)
+
+
+def several_historical_data(token_list: list, days: str):
+    """
+    token_list -> ["bitcoin", "ethereum", "fantom", "link", "litecoin"]:param
+    """
+    historical_dict = dict()
+
+    for token in token_list:
+        hist = get_historical_data(id=token, days=days, interval="daily", vs_currency="usd")
+        historical_dict[token] = hist
+    hist_df = pd.DataFrame(historical_dict)
+    return hist_df
